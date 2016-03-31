eb7ed4b25dca46083c16771bed2eebae2a7fca29
Le Phan Cong Hieu <hsblh32@gmail.com>
2016-03-31 03:18:50 +0000

    add user devise,add homepage,add signup,login,logout

---
+++ db/migrate/20160331030548_add_name_and_age.rb
@@ -0,0 +1,9 @@
+class AddNameAndAge < ActiveRecord::Migration
+  def change
+   
+     add_column :users,:name,:string
+     add_column :users,:age,:integer
+     add_index :users,:name,unique: true
+   
+  end
+end
